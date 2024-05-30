6dfb08df3bb03225ff3077b301dc809575558abb
Calvin Leepalao <calvinleepalao1@gmail.com>
2024-05-29 23:18:04 -0500

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by Calvin LeePalao on 5/29/24.
 //
 
+
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURES:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ */
+
 import SwiftUI
 
 struct ContentView: View {
