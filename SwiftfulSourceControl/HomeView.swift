15568203cdf24768f0c40378678c765fa93c602e
Calvin Leepalao <calvinleepalao1@gmail.com>
2024-05-29 23:09:31 -0500

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Calvin LeePalao on 5/29/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
