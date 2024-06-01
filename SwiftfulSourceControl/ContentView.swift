7f32fbf03f51241fe1801a8c9c8a657eb27a3cbc
Calvin Leepalao <calvinleepalao1@gmail.com>
2024-05-31 20:47:50 -0500

    [Feature] Changed house.fill to bolt.fill.

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -43,7 +43,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "house.fill")
+            Image(systemName: "bolt.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Swiftful!")
